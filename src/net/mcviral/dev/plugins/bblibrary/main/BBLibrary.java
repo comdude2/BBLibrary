22632f67fa9537009bdfbd43efef5b51f0488a63
Matt Armer <matt@comdude2.com>
2015-03-06 21:55:30 +0000

    Some changes

--- src/net/mcviral/dev/plugins/bblibrary/main/BBLibrary.java
+++ src/net/mcviral/dev/plugins/bblibrary/main/BBLibrary.java
@@ -1,5 +1,15 @@
 package net.mcviral.dev.plugins.bblibrary.main;
 
-public class BBLibrary {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BBLibrary extends JavaPlugin{
+	
+	public void onEnable(){
+		this.getLogger().info(this.getDescription().getName() + " Enabled!");
+	}
+	
+	public void onDisable(){
+		this.getLogger().info(this.getDescription().getName() + " Disabled!");
+	}
 	
 }
